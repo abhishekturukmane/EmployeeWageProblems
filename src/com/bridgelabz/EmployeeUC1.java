0a4806222c0ccbabac7a141bf5aaefeeb2b47e08
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-30 06:14:24 -0700

    Added: added UC3 Part-Time Employee wage

--- src/com/bridgelabz/EmployeeUC1.java
+++ src/com/bridgelabz/EmployeeUC1.java
@@ -2,36 +2,38 @@ package com.bridgelabz;
 
 public class EmployeeUC1 {
 
-	//UC2---DAILY WAGE
+	//UC3--- PART TIME EMPLOYEE WAGE
 	public void check()
 	{
 		//constants
+		int IS_PART_TIME=0;
 		int IS_FULL_TIME=1;
 		int EMP_RATE_PER_HOUR=20;
 		//variables
-		int empWage=0;
 		int empHrs=0;
+		int empWage=0;
 		
-		double empcheck=Math.floor(Math.random()*10)%2;//random takes value bet 0 to1
+		double empcheck=Math.floor(Math.random()*10)%2;
 		
-		if(empcheck == IS_FULL_TIME )
+		if(empcheck == IS_PART_TIME )
 		{
 			empHrs=8;
-			System.out.println("Employee hours:"+ empHrs);
-			
+			System.out.println("Part-Time hours:"+empHrs);
 		}
 		else
 		{
-			empHrs=0;
-			System.out.println("Employee hours:"+ empHrs);
+			empHrs=16;
+			System.out.println("Full-Time hours:"+empHrs);
+
 		}
-		
-		empWage=empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("The Employee Wage is:"+ empWage);
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("The EMployee Wage is: "+ empWage);
 		
 	}
 	public static void main(String[] args) {
-		EmployeeUC1 obj = new EmployeeUC1();
+		EmployeeUC1 obj=new EmployeeUC1();
 		obj.check();
 	}
+	
+		
 }
