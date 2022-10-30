ad4c32c9b35b667fab32036f81c553bdd2d9ec1d
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-30 06:17:50 -0700

    Added: added UC4 Employee Wage Using Switch Case

--- src/com/bridgelabz/EmployeeUC1.java
+++ src/com/bridgelabz/EmployeeUC1.java
@@ -1,34 +1,41 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class EmployeeUC1 {
 
-	//UC3--- PART TIME EMPLOYEE WAGE
+	//UC4--- Solve Using Switch case statement
 	public void check()
 	{
-		//constants
+		Scanner sc=new Scanner(System.in);
+		//constants 
 		int IS_PART_TIME=0;
 		int IS_FULL_TIME=1;
 		int EMP_RATE_PER_HOUR=20;
 		//variables
-		int empHrs=0;
 		int empWage=0;
+		int empHrs=0;
 		
-		double empcheck=Math.floor(Math.random()*10)%2;
-		
-		if(empcheck == IS_PART_TIME )
+		double empCheck=Math.floor(Math.random()*10)%2;
+		int i= (int)empCheck;//typecasted because floor function is in double and 
+							 //switch case dont use double
+		switch(i)
 		{
-			empHrs=8;
-			System.out.println("Part-Time hours:"+empHrs);
+			case 0 : empHrs=8;
+					 System.out.println("Employee hours: "+ empHrs);
+					 empWage=empHrs*EMP_RATE_PER_HOUR;
+					 System.out.println("Wage for Part-Time Employee is: "+ empWage);
+					 break;
+			case 1 : empHrs=16;
+			         System.out.println("EMployee hours: "+ empHrs);
+			         empWage=empHrs*EMP_RATE_PER_HOUR;
+					 System.out.println("Wage for Full-Time Employee is: "+ empWage);
+			         break;
+			         
+			 default :System.out.println("Invalid Number");
+			     		break;
+			
 		}
-		else
-		{
-			empHrs=16;
-			System.out.println("Full-Time hours:"+empHrs);
-
-		}
-		empWage=empHrs*EMP_RATE_PER_HOUR;
-		System.out.println("The EMployee Wage is: "+ empWage);
-		
 	}
 	public static void main(String[] args) {
 		EmployeeUC1 obj=new EmployeeUC1();
