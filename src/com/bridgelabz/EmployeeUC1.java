3e25e134abe6dcb830ed959dec5407353ab13e34
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-30 06:09:26 -0700

    Added: added UC2 Daily Wage problem

--- src/com/bridgelabz/EmployeeUC1.java
+++ src/com/bridgelabz/EmployeeUC1.java
@@ -2,28 +2,36 @@ package com.bridgelabz;
 
 public class EmployeeUC1 {
 
+	//UC2---DAILY WAGE
 	public void check()
 	{
 		//constants
-		int IS_PRESENT=1;
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
 		//variables
+		int empWage=0;
+		int empHrs=0;
 		
-		double empcheck=Math.floor((Math.random()*10)%2);
+		double empcheck=Math.floor(Math.random()*10)%2;//random takes value bet 0 to1
 		
-		if(empcheck == IS_PRESENT)
+		if(empcheck == IS_FULL_TIME )
 		{
-			System.out.println("Employee is Present");
+			empHrs=8;
+			System.out.println("Employee hours:"+ empHrs);
+			
 		}
 		else
 		{
-			System.out.println("Employee is absent");
+			empHrs=0;
+			System.out.println("Employee hours:"+ empHrs);
 		}
+		
+		empWage=empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("The Employee Wage is:"+ empWage);
 		
 	}
 	public static void main(String[] args) {
-		
-		EmployeeUC1 obj=new EmployeeUC1();
+		EmployeeUC1 obj = new EmployeeUC1();
 		obj.check();
-		
 	}
 }
