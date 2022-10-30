20cf12e58190425c75b9bcb559ebd63888271bf5
abhishekturukmane <abhishekturukmaneofficial98@gmail.com>
2022-10-30 06:26:42 -0700

    Added: added UC5 Wage for a Month

--- src/com/bridgelabz/EmployeeUC1.java
+++ src/com/bridgelabz/EmployeeUC1.java
@@ -4,38 +4,44 @@ import java.util.Scanner;
 
 public class EmployeeUC1 {
 
-	//UC4--- Solve Using Switch case statement
+	//UC5--- Calculating Wages for a Month
 	public void check()
 	{
-		Scanner sc=new Scanner(System.in);
-		//constants 
+		//constants
 		int IS_PART_TIME=0;
 		int IS_FULL_TIME=1;
 		int EMP_RATE_PER_HOUR=20;
+		
 		//variables
-		int empWage=0;
+		int empWage=20;
 		int empHrs=0;
+		int WorkingDays=20;
 		
 		double empCheck=Math.floor(Math.random()*10)%2;
-		int i= (int)empCheck;//typecasted because floor function is in double and 
-							 //switch case dont use double
-		switch(i)
+		
+		if(empCheck == IS_PART_TIME)
 		{
-			case 0 : empHrs=8;
-					 System.out.println("Employee hours: "+ empHrs);
-					 empWage=empHrs*EMP_RATE_PER_HOUR;
-					 System.out.println("Wage for Part-Time Employee is: "+ empWage);
-					 break;
-			case 1 : empHrs=16;
-			         System.out.println("EMployee hours: "+ empHrs);
-			         empWage=empHrs*EMP_RATE_PER_HOUR;
-					 System.out.println("Wage for Full-Time Employee is: "+ empWage);
-			         break;
-			         
-			 default :System.out.println("Invalid Number");
-			     		break;
+			empHrs=8;
+			System.out.println("Employee Hours:"+empHrs );
+		}
+		else
+		{
+			empHrs=16;
+			System.out.println("Employee Hours:"+empHrs );
+
 			
 		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Employee Wage for single day is :"+empWage );
+		
+		//calculate for 20 working days
+		int totalWage=0;
+		for(int i=1;i<=WorkingDays;i++)
+		{
+			totalWage=totalWage+empWage;
+		}
+		
+		System.out.println("Total Wage of Employee is:" + totalWage);
 	}
 	public static void main(String[] args) {
 		EmployeeUC1 obj=new EmployeeUC1();
